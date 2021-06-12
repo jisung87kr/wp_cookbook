557bfb1456f7af154b02b0c6ab229fa8bae7a903
jiseong <jisung87kr@nate.com>
2021-06-12 19:55:33 +0900

    #7 - 부모 재료 빼기

--- wp-content/themes/cookbook/page-refrigerator.php
+++ wp-content/themes/cookbook/page-refrigerator.php
@@ -5,7 +5,10 @@ get_header();
         <?php
         $args = [
             'title' => '가지고 있는 재료 선택하기',
-            'terms' => get_terms('material', ['orderby' => 'count']),
+            'terms' => get_terms('material', [
+                    'orderby' => 'count',
+                    'childless' => true
+            ]),
         ];
         get_template_part('template-parts/page/content', 'refrigerator', $args);
         ?>
