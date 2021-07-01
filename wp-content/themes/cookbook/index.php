c5e6b387ea72bf30823447650651dd38625a4efe
jiseong <jisung87kr@nate.com>
2021-07-02 00:30:02 +0900

    refs #18 - 검색에 taxonomy 추가

--- wp-content/themes/cookbook/index.php
+++ wp-content/themes/cookbook/index.php
@@ -1,6 +1,7 @@
 <?php
 get_header();
 global $POSTTYPES;
+$taxArr = ['cookTag', 'cookCategory', 'material'];
 $args = array(
     'post_type' => $POSTTYPES,
     'paged' => get_query_var('paged') ? get_query_var('paged') : 1,
@@ -8,10 +9,40 @@ $args = array(
 );
 
 if(get_query_var('s')){
+    function advance_search_where($where){
+        global $wpdb;
+        if (is_search())
+            $where .= "OR (t.name LIKE '%".get_search_query()."%' AND {$wpdb->posts}.post_status = 'publish')";
+        return $where;
+    }
+
+    function advance_search_join($join){
+        global $wpdb;
+        if (is_search())
+            $join .= "LEFT JOIN {$wpdb->term_relationships} tr ON {$wpdb->posts}.ID = tr.object_id INNER JOIN {$wpdb->term_taxonomy} tt ON tt.term_taxonomy_id=tr.term_taxonomy_id INNER JOIN {$wpdb->terms} t ON t.term_id = tt.term_id";
+        return $join;
+    }
+
+    function advance_search_groupby($groupby){
+        global $wpdb;
+        // we need to group on post ID
+        $groupby_id = "{$wpdb->posts}.ID";
+
+        if(!is_search() || strpos($groupby, $groupby_id) !== false) return $groupby;
+
+        // groupby was empty, use ours
+        if(!strlen(trim($groupby))) return $groupby_id;
+
+        // wasn't empty, append ours
+        return $groupby.", ".$groupby_id;
+    }
+
+    add_filter('posts_where','advance_search_where');
+    add_filter('posts_join', 'advance_search_join');
+    add_filter('posts_groupby', 'advance_search_groupby');
+
     $args['s'] = get_query_var('s');
 }
-
-$taxArr = ['cookTag', 'cookCategory', 'material'];
 
 foreach ($taxArr as $index => $item) {
     if(is_tax($item)){
